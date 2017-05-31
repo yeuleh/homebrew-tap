41d9b65d9c60593390c9b00a125c219ca9440108
Yu Le <yule@cn.fujitsu.com>
2017-05-31 15:57:39 +0800

    I don\'t know why but "#{var}" seems not working correctly.

--- schemaspy.rb
+++ schemaspy.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Schemaspy < Formula
   homepage 'http://schemaspy.sourceforge.net'
   version '5.0.0'
-  url 'https://jaist.dl.sourceforge.net/project/schemaspy/schemaspy/SchemaSpy%205.0.0/schemaSpy_#{version}.jar'
+  url 'https://jaist.dl.sourceforge.net/project/schemaspy/schemaspy/SchemaSpy%205.0.0/schemaSpy_5.0.0.jar'
   sha256 '4f46ec1647447a9282fd24db93ae797cf9d8f8ebef07f0e10c70ca41af6b9a43'
 
   def install
